aa54e819d3033fddb371b773201989151bc7b4d0
eunyeoung22 <ey0928337@naver.com>
2023-01-02 20:26:17 +0900

    Create keras07_mlp2_homework.py

---
+++ keras/keras07_mlp2_homework.py
@@ -0,0 +1,37 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+#1.데이터
+x = np.array([[1,2,3,4,5,6,7,8,9,10],
+             [1,1,1,1,2,1.3,1.4,1.5,1.6,1.4],
+             [9,8,7,6,5,4,3,2,1,0]])
+y = np.array([2,4,6,8,10,12,14,16,18,20])
+
+print(x.shape)
+print(y.shape)
+
+x = x.T
+print(x.shape)
+
+model = Sequential()
+model.add(Dense(5, input_dim=3))
+model.add(Dense(4))
+model.add(Dense(3))
+model.add(Dense(2))
+model.add(Dense(1))
+
+model.compile(loss='mae' , optimizer='adam')
+model.fit(x, y, epochs=100, batch_size=2)
+
+loss=model.evaluate(x,y)
+print('loss : ' , loss)
+
+result = model.predict([[10,1.4,0]])
+print('[10,1.4,0]의 예측 값 : ', result)
+
+"""
+결과 값 
+ loss : 0.0867
+ 예측 값 : 19.83238
+"""
