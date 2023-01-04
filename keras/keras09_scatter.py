63d041ea7131e524a1021e22ff8de82c6e9674e6
eunyeoung22 <ey0928337@naver.com>
2023-01-04 18:17:55 +0900

    Update keras09_scatter.py

--- keras/keras09_scatter.py
+++ keras/keras09_scatter.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 x= np.array(range(1,21))
 y= np.array([1,2,4,3,5,7,9,3,8,12,13,8,14,15,9,6,17,23,21,20])
 
+
 x_train, x_test, y_train, y_test = train_test_split(x,y,
     train_size=0.7, shuffle=True, random_state=123
 )
@@ -30,7 +31,7 @@ model.fit(x_train, y_train, epochs=200, batch_size=1)
 loss = model.evaluate(x_test, y_test)
 print('loss : ', loss)
 y_predict = model.predict(x)
-print('예측값 : ' , y_predict)
+
 
 import matplotlib.pyplot as plt
 plt.scatter(x,y)
