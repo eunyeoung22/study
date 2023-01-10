07b4e46455c353dc1d66e440da7abd97694ed1aa
eunyeoung22 <ey0928337@naver.com>
2023-01-10 19:14:51 +0900

    Create keras22_softmax2_wine.py

---
+++ keras/keras22_softmax2_wine.py
@@ -0,0 +1,47 @@
+import numpy as np
+from sklearn.datasets import load_wine
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+
+#1. 데이터
+datasets = load_wine()
+x = datasets.data
+y = datasets.target
+y = to_categorical(y)
+
+# print(x.shape, y.shape) #(178, 13) (178,)
+# print(np.unique(y)) #[0 1 2]
+# print(np.unique(y, return_counts=True)) #(array([0, 1, 2]), array([59, 71, 48], dtype=int64))
+# print(datasets.DESCR)
+# print(datasets.feature_names)
+
+x_train, x_test, y_train, y_test = train_test_split(
+                x, y, shuffle=True,
+                random_state=123,
+                test_size=0.2,
+                stratify=y
+)
+#2. 모델 구성
+model = Sequential()
+model.add(Dense(10000, activation='linear', input_shape = (13,)))
+model.add(Dense(100, activation='linear'))
+model.add(Dense(100, activation='linear'))
+model.add(Dense(100, activation='linear'))
+model.add(Dense(100, activation='linear'))
+model.add(Dense(100, activation='linear'))
+model.add(Dense(100, activation='linear'))
+model.add(Dense(50, activation='linear'))
+model.add(Dense(3, activation='softmax'))
+
+#3. 컴파일, 훈련
+model.compile(loss = 'categorical_crossentropy', optimizer='adam',
+              metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=10000, batch_size=1,
+          validation_split=0.2, verbose=1)
+
+#4. 평가, 예측
+loss, accuracy = model.evaluate(x_test, y_test)
+print('loss: ', loss)
+print('accuracy : ', accuracy)
