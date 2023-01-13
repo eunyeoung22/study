bb57421639da899bfa91c1850f534b565ca85446
eunyeoung22 <ey0928337@naver.com>
2023-01-13 20:00:16 +0900

    Update keras31_dropout06.cancer.py

--- keras/keras31_dropout06.cancer.py
+++ keras/keras31_dropout06.cancer.py
@@ -48,35 +48,49 @@ x_test = scaler.transform(x_test)
 input1 = Input(shape=(30,))
 dence1 = Dense(50, activation= 'linear')(input1)
 dence2 = Dense(40, activation= 'relu')(dence1)
-dence3 = Dense(30, activation= 'relu')(dence2)
+drop2 = Dropout(0.5)(dence2)
+dence3 = Dense(30, activation= 'relu')(drop2)
 dence4 = Dense(10, activation= 'relu')(dence3)
 output1 = Dense(1, activation= 'sigmoid')(dence4)
-model1 = Model(inputs = input1, outputs = output1)
-model1.summary()
+model = Model(inputs = input1, outputs = output1)
+model.summary()
 # Total params: 5,141
 # Trainable params: 5,141
 # Non-trainable params: 0
 
 #3. 컴파일, 훈련
-model1.compile(loss = 'binary_crossentropy', optimizer= 'adam', metrics=['accuracy']) # 이진분류에선 꼭 이 훈련 쓰기
-from tensorflow.keras.callbacks import EarlyStopping # 
-earlyStopping = EarlyStopping(monitor= 'val_loss' ,
+model.compile(loss = 'binary_crossentropy', optimizer= 'adam', metrics=['accuracy']) # 이진분류에선 꼭 이 훈련 쓰기
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint 
+es = EarlyStopping(monitor= 'val_loss' ,
                               mode='min',
                               patience = 300, 
                               restore_best_weights=True, # patience 10번 중 마지막 최소값이 아닌 그중 제일 최소값 반환
                               verbose=1
                               )
-model1.fit(x_train, y_train, epochs=10000, batch_size= 15,
-          validation_split=0.2,
-          callbacks=[earlyStopping],
-          verbose=1)
+import datetime
+date = datetime.datetime.now()
+print(date) #2023-01-12 14:57:51.908060
+print(type(date)) #class 'datetime.datetime' 
+date = date.strftime("%m%d_%H%M") #0112_1502 ->스트링 문자열 형식으로 바꿔주기
+print(date)
+print(type(date)) #<class 'str'>->스트링 문자열 형태임 
+
+filepath = './_save/MCP/'
+filename = '{epoch:04d}-{val_loss:.4f}.hdf5' # epoch는 정수 4자리까지 val_loss는 소수점 4자리 이하까지 .hdf5 파일 만들기
+
+mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1,
+                      save_best_only=True, #가장 좋은 지점을 저장
+                      filepath= filepath +'k31_05_'+ '_' + date + '_' + filename) #파일 저장 경로 지정                
+                    #   filepath= path +'MCP/keras30_ModelCheckPoint13.hdf5') #파일 저장 경로 지정
+model.fit(x_train, y_train, epochs=3000, batch_size=32, validation_split=0.2,
+                callbacks=[es, mcp], verbose=1)
 
 #4. 평가, 예측
-loss, accuracy = model1.evaluate(x_test, y_test)
+loss, accuracy = model.evaluate(x_test, y_test)
 print('loss : ', loss)
 print('accuracy : ', accuracy)
 
-y_predict = model1.predict(x_test)
+y_predict = model.predict(x_test)
 intarr = list(map(int, y_predict)) 
 print(intarr[:10])  #--> 정수형으로 바꿔줘야함 
 print(y_test[:10])
