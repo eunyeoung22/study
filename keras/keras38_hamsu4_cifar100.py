2bf9c210f85dc9109d8d9d23031e0bf1c13a0446
eunyeoung22 <ey0928337@naver.com>
2023-01-25 20:23:13 +0900

    Create keras38_hamsu4_cifar100.py

---
+++ keras/keras38_hamsu4_cifar100.py
@@ -0,0 +1,111 @@
+import numpy as np
+from tensorflow.keras.models import Sequential,Model
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, MaxPooling2D, Input, Dropout
+from tensorflow.keras.datasets import cifar100
+
+
+(x_train, y_train), (x_test, y_test) = cifar100.load_data()
+
+print(x_train.shape, y_train.shape) #(50000, 32, 32, 3) (50000,1)
+print(x_test.shape, y_test.shape)   #(10000, 32, 32, 3) (10000,1)
+
+x_train = x_train.reshape(50000,32*32*3)
+x_test = x_test.reshape(10000,32*32*3)
+
+print(np.unique(y_train, return_counts=True))
+# #(array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16,
+#        17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
+#        34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+#        51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67,
+#        68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84,
+#        85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]), array([500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500,
+#        500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500,
+#        500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500,
+#        500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500,
+#        500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500,
+#        500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500,
+#        500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500,
+#        500, 500, 500, 500, 500, 500, 500, 500, 500], dtype=int64))
+
+
+#2. 모델구성(순차형)
+# model = Sequential()
+# model.add(Conv2D(filters=200, kernel_size=(2,2), input_shape = (32,32,3), 
+#                   activation = 'relu',
+#                   padding = 'same')) 
+# model.add(Conv2D(filters=100, kernel_size=(2,2)))
+# model.add(MaxPooling2D())
+# model.add(Conv2D(filters=100, kernel_size=(2,2))) 
+# model.add(Flatten()) 
+# model.add(Dense(100, activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(100, activation='relu'))
+# model.add(Dense(100, activation='relu'))
+# model.add(Dense(100, activation='relu'))
+# model.add(Dense(100, activation='relu'))
+# model.add(Dense(100, activation='relu'))
+# model.add(Dense(100, activation='relu'))
+# model.add(Dense(100, activation='relu'))
+# model.add(Dense(100, activation='relu'))
+# model.add(Dropout(0.1))
+# model.add(Dense(100, activation='relu'))         
+# model.add(Dense(100, activation='softmax'))
+
+#2. 모델 구성(함수형)
+input1 = Input(shape=(32*32*3,))
+dence1 = Dense(128, activation= 'relu')(input1)
+dence2 = Dense(64, activation= 'relu')(dence1)
+dence3 = Dense(64, activation= 'relu')(dence2)
+dence4 = Dense(100, activation= 'linear')(dence3)
+dence5 = Dense(100, activation= 'linear')(dence4)
+dence6 = Dense(200, activation= 'linear')(dence5)
+dence7 = Dense(100, activation= 'linear')(dence6)
+dence8 = Dense(100, activation= 'relu')(dence7)
+dence9 = Dense(100, activation= 'relu')(dence8)
+dence10 = Dense(100, activation= 'relu')(dence9)
+output1 = Dense(10, activation= 'softmax')(dence10)
+model = Model(inputs = input1, outputs = output1)
+
+#3. 컴파일, 훈련
+model.compile(loss ='sparse_categorical_crossentropy', optimizer='adam', 
+            metrics=['acc'])
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+es = EarlyStopping(monitor='val_loss',
+                   mode='min',
+                   patience=50,
+                   verbose=1)
+
+# import datetime
+# date = datetime.datetime.now()
+# print(date) #2023-01-12 14:57:51.908060
+# print(type(date)) #class 'datetime.datetime' 
+# date = date.strftime("%m%d_%H%M") #0112_1502 ->스트링 문자열 형식으로 바꿔주기
+# print(date)
+# print(type(date))
+
+# filepath = './_save/MCP/'
+# filename = '{epoch:04d}-{val_loss:.4f}.hdf5' 
+
+# mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1,
+#                       save_best_only=True, 
+#                       filepath= filepath +'k34_3_'+ '_' + date + '_' + filename)               
+#                     #   filepath= path +'MCP/keras30_ModelCheckPoint13.hdf5') #파일 저장 경로 지정
+model.fit(x_train, y_train, epochs= 50, verbose=1, batch_size=32, validation_split=0.2,
+          callbacks=[es])
+
+#4. 평가, 예측
+results = model.evaluate(x_test, y_test)
+print('loss : ', results[0])
+print('acc : ', results[1])
+
+
+"""
+loss :  4.605196952819824
+acc :  0.009999999776482582
+
+loss :  3.147641181945801
+acc :  0.25780001282691956
+
+
+"""
+
